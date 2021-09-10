1c53ba5b16a12443c36181b6649fe35b525c61e2
Oleksandr Bazurin <aqua.user.ta@gmail.com>
2021-09-10 22:05:48 +0300

    first commit

---
+++ Presistance/Migrations/20210910183811_UpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Presistance.Migrations
+{
+    public partial class UpdateModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descriptiion",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Descriptiion");
+        }
+    }
+}
